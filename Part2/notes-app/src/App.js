117e4a232acfbe5b639a66663ec97558f86bd612
poweredbynano <nanolinksgh@gmail.com>
2023-08-09 18:10:06 +0100

    Solution for notes-app; refactoring axios and putting things in the right directory

--- Part2/notes-app/src/App.js
+++ Part2/notes-app/src/App.js
@@ -1,25 +1,33 @@
 import { useState, useEffect } from "react"
-import axios from "axios"
 import Note from "./components/Note"
+import noteService from "./services/notes"
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(false)
 
+  // useEffect(() => {
+  //   console.log('effect')
+  //   axios
+  //   .get('http://localhost:3001/notes')
+  //   .then(response => {
+  //     console.log('promise fulfilled')
+  //     setNotes(response.data)
+  //   })
+  // }, [])
+
   useEffect(() => {
-    console.log('effect')
-    axios
-    .get('http://localhost:3001/notes')
-    .then(response => {
-      console.log('promise fulfilled')
-      setNotes(response.data)
+    noteService
+    .getAll()
+    .then(initialNotes => {
+      setNotes(initialNotes)
     })
   }, [])
 
   console.log('render', notes.length, 'notes')
 
-  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -31,10 +39,16 @@ const App = () => {
       important: Math.random() < 0.5
     }
 
-    axios
-    .post('http://localhost:3001/notes', noteObject)
-    .then(response => {
-      setNotes(notes.concat(response.data))
+    // axios
+    // .post('http://localhost:3001/notes', noteObject)
+    // .then(response => {
+    //   setNotes(notes.concat(response.data))
+    //   setNewNote('')
+    // })
+    noteService
+    .create(noteObject)
+    .then(returnedNote => {
+      setNotes(notes.concat(returnedNote))
       setNewNote('')
     })
   }
@@ -45,10 +59,19 @@ const App = () => {
     const changedNote = {...note, important: !note.important}
     console.log(`importance of ${note} ${id} at this url: ${url} needs to be toggled to become ${changedNote}`)
 
-    axios
-    .put(url, changedNote)
-    .then(response => {
-      setNotes(notes.map(n => n.id !== id ? n : response.data))
+    // axios
+    // .put(url, changedNote)
+    // .then(response => {
+    //   setNotes(notes.map(n => n.id !== id ? n : response.data))
+    // })
+    noteService
+    .update(id, changedNote)
+    .then(returnedNote => {
+      setNotes(notes.map(n => n.id !== id ? n : returnedNote))
+    })
+    .catch(error => {
+      alert(`the note ${note.content} was already deleted from server`)
+      setNotes(notes.filter(n => n.id !== id))
     })
   }
   const handleNoteChange = (event) => {
