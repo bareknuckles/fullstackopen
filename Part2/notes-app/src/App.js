dc5a09ecb8ed8ae9c6f0ad696ec353dfd91b6a04
poweredbynano <nanolinksgh@gmail.com>
2023-08-09 17:27:35 +0100

    Solution for notes-app axios features and toggle feature

--- Part2/notes-app/src/App.js
+++ Part2/notes-app/src/App.js
@@ -26,15 +26,31 @@ const App = () => {
     console.log('Form clicked', event.target)
 
     const noteObject = {
+      id: notes.length + 1,
       content: newNote,
-      important: Math.random() < 0.5,
-      id: notes.length + 1
+      important: Math.random() < 0.5
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote(' ')
+    axios
+    .post('http://localhost:3001/notes', noteObject)
+    .then(response => {
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+    })
   }
 
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+    console.log(`importance of ${note} ${id} at this url: ${url} needs to be toggled to become ${changedNote}`)
+
+    axios
+    .put(url, changedNote)
+    .then(response => {
+      setNotes(notes.map(n => n.id !== id ? n : response.data))
+    })
+  }
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
@@ -50,7 +66,7 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note}/>  
+          <Note key={note.id} note={note} toggleImportanceOf={() => toggleImportanceOf(note.id)}/>  
         )}
       </ul>
       <form onSubmit={addNote}>
--- Part2/notes-app/src/components/Note.js
+++ Part2/notes-app/src/components/Note.js
@@ -1,5 +1,12 @@
-const Note = ({note}) => {
-    return <li>{note.content}</li>
+const Note = ({note, toggleImportanceOf}) => {
+    const label = note.important ? 'Make important' : 'Make not important'
+    
+    return (
+    <li>
+        {note.content}
+        <button onClick={toggleImportanceOf}>{label}</button>
+    </li>
+    )
 }
 
 export default Note;
